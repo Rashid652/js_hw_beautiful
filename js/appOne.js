0ff114ec163a0d1a0a5caa4cf36c083a6cc4eaad
rashaprogramming <70647526+Rashid652@users.noreply.github.com>
2022-06-05 00:17:48 +0500

    Update appOne.js

--- js/appOne.js
+++ js/appOne.js
@@ -1,59 +1,14 @@
-let dataUsers = {};
-let id = 0;
+user = {};
 
-dataUsers['user_1'] = {
-    name: prompt('Введите имя!', 'Иван'),
-    age: +prompt('Введите возраст!', '35')
+for(let l=1;l<=10;l++){
+    user[l] = {}
+    user[l].name = prompt('Введите имя');
+    user[l].age = +prompt('Введите возраст');
 }
 
-dataUsers['user_2'] = {
-    name: prompt('Введите имя!', 'Иван'),
-    age: +prompt('Введите возраст!', '35')
+for(let key in user){
+    console.log(`Пользователь ${key}`);
+    console.log(`Ваше имя ${user[key].name}`);
+    console.log(`Ваш возраст ${user[key].age}`);
 }
-
-dataUsers['user_3'] = {
-    name: prompt('Введите имя!', 'Иван'),
-    age: +prompt('Введите возраст!', '35')
-}
-
-dataUsers['user_4'] = {
-    name: prompt('Введите имя!', 'Иван'),
-    age: +prompt('Введите возраст!', '35')
-}
-
-dataUsers['user_5'] = {
-    name: prompt('Введите имя!', 'Иван'),
-    age: +prompt('Введите возраст!', '35')
-}
-
-dataUsers['user_6'] = {
-    name: prompt('Введите имя!', 'Иван'),
-    age: +prompt('Введите возраст!', '35')
-}
-
-dataUsers['user_7'] = {
-    name: prompt('Введите имя!', 'Иван'),
-    age: +prompt('Введите возраст!', '35')
-}
-
-dataUsers['user_8'] = {
-    name: prompt('Введите имя!', 'Иван'),
-    age: +prompt('Введите возраст!', '35')
-}
-
-dataUsers['user_9'] = {
-    name: prompt('Введите имя!', 'Иван'),
-    age: +prompt('Введите возраст!', '35')
-}
-
-dataUsers['user_10'] = {
-    name: prompt('Введите имя!', 'Иван'),
-    age: +prompt('Введите возраст!', '35')
-}
-
-for(let key in dataUsers) {
-    console.log(`Пользователь ${++id}`);
-    for(let newKey in dataUsers[key]) {
-        console.log(dataUsers[key][newKey]);
-    }
-}
+console.log(user);
